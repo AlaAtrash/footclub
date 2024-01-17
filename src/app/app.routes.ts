2ec4b1a382c97af86e30a1e8bf341660627ed18a
Ala <atrash.ala@gmail.com>
2024-01-17 20:39:57 +0100

    feat: adding the routing options

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { HomeComponent } from './home/home.component'
 import { ContactComponent } from './contact/contact.component'; 
 
@@ -11,3 +12,12 @@ export const routes: Routes = [
 
 export const routes: Routes = [];
 >>>>>>> 81fe02d (first commit)
+=======
+import { HomeComponent } from './home/home.component'
+import { ContactComponent } from './contact/contact.component'; 
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+];
+>>>>>>> 11840b5 (feat: adding the routing options)
