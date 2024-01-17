c447df95a86b74c23b53e9b087cbfe28efc0344e
Ala <atrash.ala@gmail.com>
2024-01-17 20:32:52 +0100

    [skip ci]feat: Adding home and contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,9 @@ import { ContactComponent } from './contact/contact.component';
   selector: 'nfc-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, HomeComponent, ContactComponent],
+
   imports: [CommonModule, RouterOutlet],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
