82a4c1dd0c53dea59c8782e1cd8772f9e87a01e9
Ala <atrash.ala@gmail.com>
2024-01-17 20:32:52 +0100

    [skip ci]feat: Adding home and contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,28 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { HomeComponent } from './home/home.component'
 import { ContactComponent } from './contact/contact.component'; 
 =======
 >>>>>>> 81fe02d (first commit)
+=======
+import { HomeComponent } from './home/home.component'
+import { ContactComponent } from './contact/contact.component'; 
+>>>>>>> 3498dea ([skip ci]feat: Adding home and contact component)
 
 @Component({
   selector: 'nfc-root',
   standalone: true,
 <<<<<<< HEAD
+<<<<<<< HEAD
   imports: [CommonModule, RouterOutlet, HomeComponent, ContactComponent],
 =======
   imports: [CommonModule, RouterOutlet],
 >>>>>>> 81fe02d (first commit)
+=======
+  imports: [CommonModule, RouterOutlet, HomeComponent, ContactComponent],
+>>>>>>> 3498dea ([skip ci]feat: Adding home and contact component)
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 81fe02d (first commit)
+=======
+
+>>>>>>> 3498dea ([skip ci]feat: Adding home and contact component)
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
