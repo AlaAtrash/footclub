0fb2fcfd685037d13b28190af5dee17953e4eb68
Ala <atrash.ala@gmail.com>
2024-01-17 20:39:57 +0100

    feat: adding the routing options

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,4 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 2ec4b1a (feat: adding the routing options)
-import { HomeComponent } from './home/home.component'
-import { ContactComponent } from './contact/contact.component'; 
-export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'contact', component: ContactComponent },
-];
-
-
-export const routes: Routes = [];
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 81fe02d (first commit)
-=======
 import { HomeComponent } from './home/home.component'
 import { ContactComponent } from './contact/contact.component'; 
 
@@ -26,5 +6,9 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
 ];
->>>>>>> 11840b5 (feat: adding the routing options)
->>>>>>> 2ec4b1a (feat: adding the routing options)
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-<<<<<<< HEAD
-=======
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 81fe02d (first commit)
-=======
-
->>>>>>> 3498dea ([skip ci]feat: Adding home and contact component)
->>>>>>> 82a4c1d ([skip ci]feat: Adding home and contact component)
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
