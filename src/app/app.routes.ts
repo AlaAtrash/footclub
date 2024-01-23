58ce70bd40c9530ae5cfef9ef7bee4f88ba63a93
Fleurdel34 <betdevelweb0953@gmail.com>
2024-01-23 18:53:56 +0100

    CSS mis en place pour la page Contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { HomeComponent } from './home/home.component'
 import { ContactComponent } from './contact/contact.component'; 
 
@@ -9,24 +6,3 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
 ];
-=======
-
-export const routes: Routes = [];
->>>>>>> 81fe02d (first commit)
-=======
-import { HomeComponent } from './home/home.component'
-import { ContactComponent } from './contact/contact.component'; 
-
-=======
-import { HomeComponent } from './home/home.component'
-import { ContactComponent } from './contact/contact.component'; 
-
->>>>>>> 7e37f4f (feat: adding the routing options)
-export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'contact', component: ContactComponent },
-];
-<<<<<<< HEAD
->>>>>>> 11840b5 (feat: adding the routing options)
-=======
->>>>>>> 7e37f4f (feat: adding the routing options)
