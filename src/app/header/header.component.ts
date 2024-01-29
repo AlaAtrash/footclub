ded7f0f251ae9fce827576be58527dd6f1af386f
Fleurdel34 <betdevelweb0953@gmail.com>
2024-01-29 20:21:36 +0100

    Creation components header et footer

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nfc-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
