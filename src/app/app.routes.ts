8cfbe271af37ee0ffc9c32dfd8aa3c82f81d14f5
Fleurdel34 <betdevelweb0953@gmail.com>
2024-02-07 14:19:06 +0100

    ajout des components header et footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 import { ContactComponent } from './contact/contact.component'; 
+import { HeaderComponent } from './header/header.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'header', component: HeaderComponent},
+  
 ];
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'nfc-contact',
-  standalone: true,
-  imports: [],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+    selector: 'nfc-contact',
+    standalone: true,
+    templateUrl: './contact.component.html',
+    styleUrl: './contact.component.css',
 })
 export class ContactComponent {
 
