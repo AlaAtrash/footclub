c712b0e2c49baaa9df16d690663ee5cc28acf92b
Fleurdel34 <betdevelweb0953@gmail.com>
2024-02-07 14:19:06 +0100

    ajout des components header et footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 import { ContactComponent } from './contact/contact.component'; 
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
-  selector: 'nfc-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, ContactComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'nfc-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HomeComponent, ContactComponent, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'footclub';
