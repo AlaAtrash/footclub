86d2a6637f3d9e6eb07efcca884de482847da6d6
Fleurdel34 <betdevelweb0953@gmail.com>
2024-02-09 08:37:33 +0100

    utilisation de formgroup

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,22 +1,21 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import {  FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'nfc-contact',
     standalone: true,
     templateUrl: './contact.component.html',
     styleUrl: './contact.component.css',
+    imports: [ReactiveFormsModule],
 })
 export class ContactComponent {
-
-}
-
-export class ProfileEditorComponent {
     profileForm = new FormGroup({
+    firstName: new FormControl(''),
     name: new FormControl(''),
-    firstmame: new FormControl(''),
     email: new FormControl(''),
     telephone: new FormControl(''),
-    
-    });
+
+});
 }
+
+
