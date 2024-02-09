598d43b2a6d67a01a38b571e6947b0f3eadb6476
Fleurdel34 <betdevelweb0953@gmail.com>
2024-02-09 20:12:01 +0100

    procedure FormGroup testée

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
-import {  FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
     selector: 'nfc-contact',
     standalone: true,
-    templateUrl: './contact.component.html',
+    templateUrl: './contact.component.html',        
     styleUrl: './contact.component.css',
     imports: [ReactiveFormsModule],
 })
 export class ContactComponent {
     profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    name: new FormControl(''),
-    email: new FormControl(''),
-    telephone: new FormControl(''),
-
+    name : new FormControl(''),
+    firstname : new FormControl(''),
 });
+onSubmit() {
+   
+    console.warn(this.profileForm.value);
+  }
 }
 
 
